e76adc6f73870d5b71cb80d71bee7de36497e633
Yilun Qian <qianyilun@gmail.com>
2017-10-29 20:21:59 -0700

    optimize for week 44 availability

--- persistence-with-jdbc/src/com/allen/QueueDays.java
+++ persistence-with-jdbc/src/com/allen/QueueDays.java
@@ -7,19 +7,19 @@ public class QueueDays {
 	public static Map<String, Double> hash = new HashMap<>();
 
 	public static void initHash() {
-		hash.put("Alex", 1.5);
+		hash.put("Alex", (double) 4);
 		hash.put("Allen", (double) 5);
 		hash.put("April", (double) 3);
-		hash.put("Graham", (double) 4);
-		hash.put("Hitomi", (double) 2);
+		hash.put("Graham", (double) 5);
+		hash.put("Hitomi", (double) 2.5);
 		hash.put("John H", (double) 0);
 		double johnL = 0.5;
-		hash.put("John L", (double) 3 * johnL);
-		hash.put("Julie", (double) 0);
-		hash.put("Leila", (double) 2);
-		hash.put("Marc", (double) 2);
-		hash.put("Pedro", (double) 4);
-		hash.put("Stefan", (double) 5);
+		hash.put("John L", (double) 0 * johnL);
+		hash.put("Julie", (double) 3);
+		hash.put("Leila", (double) 0);
+		hash.put("Marc", (double) 4);
+		hash.put("Pedro", (double) 3);
+		hash.put("Stefan", (double) 4);
 		double yvonne = 0.75;
 		hash.put("Yvonne", (double) 3 * yvonne);
 	}
