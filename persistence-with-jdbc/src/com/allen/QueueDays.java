0e8147f3ad9f95ead009e1cc7a33055ffdd00629
Yilun Qian <qianyilun@gmail.com>
2017-11-05 13:54:22 -0800

    update to week45 availability

--- persistence-with-jdbc/src/com/allen/QueueDays.java
+++ persistence-with-jdbc/src/com/allen/QueueDays.java
@@ -9,18 +9,18 @@ public class QueueDays {
 	public static void initHash() {
 		hash.put("Alex", (double) 3);
 		hash.put("Allen", (double) 4);
-		hash.put("April", (double) 3);
-		hash.put("Graham", (double) 3);
+		hash.put("April", (double) 4);
+		hash.put("Graham", (double) 4);
 		hash.put("Hitomi", (double) 2.5);
-		hash.put("John H", (double) 0);
+		hash.put("John H", (double) 3);
 		double johnL = 0.5;
-		hash.put("John L", (double) 0 * johnL);
-		hash.put("Julie", (double) 2);
-		hash.put("Leila", (double) 0);
+		hash.put("John L", (double) 4 * johnL);
+		hash.put("Julie", (double) 3);
+		hash.put("Leila", (double) 3);
 		hash.put("Marc", (double) 3);
-		hash.put("Pedro", (double) 3);
-		hash.put("Stefan", (double) 1);
+		hash.put("Pedro", (double) 5);
+		hash.put("Stefan", (double) 4);
 		double yvonne = 0.75;
-		hash.put("Yvonne", (double) 2 * yvonne);
+		hash.put("Yvonne", (double) 4 * yvonne);
 	}
 }
