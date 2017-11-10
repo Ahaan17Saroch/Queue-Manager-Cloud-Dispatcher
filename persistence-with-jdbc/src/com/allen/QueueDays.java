50ed342177292999c3ad0c2682b491c9e8980e70
Yilun Qian <qianyilun@gmail.com>
2017-11-09 17:12:53 -0800

    add functionality
    1. % usage issue
    2. check if interface issue resolved indeed (like ms)

--- persistence-with-jdbc/src/com/allen/QueueDays.java
+++ persistence-with-jdbc/src/com/allen/QueueDays.java
@@ -7,21 +7,25 @@ public class QueueDays {
 	public static Map<String, Double> hash = new HashMap<>();
 
 	public static void initHash() {
-		hash.put("Alex", (double) 3);
-		hash.put("Allen", (double) 4);
-		hash.put("April", (double) 3);
-		hash.put("Graham", (double) 4);
-		hash.put("Hitomi", (double) 2.5);
-		hash.put("John H", (double) 3);
+		hash.put("Alex", (double) 5);
+		hash.put("Allen", (double) 5);
+		hash.put("April", (double) 5);
+		hash.put("Graham", (double) 5);
+		hash.put("Hitomi", (double) 5);
+		hash.put("John H", (double) 5);
 		double johnL = 0.5;
-		hash.put("John L", (double) 4 * johnL);
-		hash.put("Julie", (double) 3);
-		hash.put("Leila", (double) 3);
-		hash.put("Marc", (double) 3);
-		hash.put("Pedro", (double) 4);
-		hash.put("Stefan", (double) 4);
+		hash.put("John L", (double) 5 * johnL);
+		hash.put("Julie", (double) 5);
+		hash.put("Leila", (double) 5);
+		hash.put("Marc", (double) 5);
+		hash.put("Pedro", (double) 5);
+		hash.put("Stefan", (double) 5);
 		double yvonne = 0.75;
 		hash.put("Yvonne", (double) 4 * yvonne);
+	}
+	
+	public static void changeValue(String name, double value) {
+		hash.put(name, value);
 	}
 	
 	public static void minusValue(String name) {
---
+++ persistence-with-jdbc/src/com/allen/Rcc.java
@@ -0,0 +1,106 @@
+package com.allen;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Submit
+ */
+public class Rcc extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Rcc() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().println("<html>");
+		response.getWriter().println("<head><title> Edit Availability</title></head>");
+		response.getWriter().println("<body>");
+		
+		response.getWriter().println("<center><form action=\"\" method=\"post\">" + "Name:<input type=\"text\" name=\"name\">"
+                + "&nbsp;Value:<input type=\"text\" name=\"value\">"
+                + "&nbsp;<input type=\"submit\" value=\"Update\">" + "</form></center>");
+
+		
+		
+		response.getWriter().println(
+                "<p><center><table width=50% border=\"1\">");
+        	
+        response.getWriter().println("<tr><th>Name</th><th>Current Queue Days</th><th>%usage</th></tr>");	
+	
+        
+		displayTable(response);
+		
+		response.getWriter().println("</tr></table></center></p>");
+		
+		
+		response.getWriter().println("<a style=\"color:blue\" href=\"nw\" >Return to Home</a>");
+		
+        
+        response.getWriter().println("</body>");
+		response.getWriter().println("</html>");
+	}
+	
+	private void displayTable(HttpServletResponse response) throws IOException {
+		Integer index = 1;
+		writeRow(index, response, "Alex", "1.00");
+		writeRow(index, response, "Allen", "1.00");
+		writeRow(index, response, "April", "1.00");
+		writeRow(index, response, "Graham", "1.00");
+		writeRow(index, response, "Hitomi", "1.00");
+		writeRow(index, response, "John H", "1.00");
+		writeRow(index, response, "John L", "0.5");
+		writeRow(index, response, "Julie", "1.00");
+		writeRow(index, response, "Leila", "1.00");
+		writeRow(index, response, "Marc", "1.00");
+		writeRow(index, response, "Pedro", "1.00");
+		writeRow(index, response, "Stefan", "1.00");
+		writeRow(index, response, "Yvonne", "0.75");
+	}
+
+	private void writeRow(Integer index, HttpServletResponse response, String name, String usage) throws IOException {
+		// TODO Auto-generated method stub
+		
+		response.getWriter().println("<tr><td height=\"30\"><center>" + name + "</center></td>");
+		response.getWriter().println("<td height=\"30\"><center>" + QueueDays.getValue(name) + "</center></td>");
+		response.getWriter().println("<td height=\"30\"><center>" + usage + "</center></td></tr>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name").trim();
+        String value = request.getParameter("value").trim();
+        if (name != null && value != null) {
+        	if (!name.equals("Alex") || !name.equals("Allen")
+        			|| !name.equals("April") || !name.equals("Graham")
+        			|| !name.equals("Hitomi") || !name.equals("John H")
+        			|| !name.equals("John L") || !name.equals("Julie")
+        			|| !name.equals("Leila") || !name.equals("Marc")
+        			|| !name.equals("Pedro") || !name.equals("Stefan")
+        			|| !name.equals("Yvonne")) {
+	        	double v = Double.parseDouble(value);
+	        	QueueDays.changeValue(name, v);
+        	}
+        } 
+
+        doGet(request, response);
+	}
+
+}
