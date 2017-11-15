a966afe712396d7b89a288dad0b8374f4605ec67
Allen's VM <qianyilun@gmail.com>
2017-11-14 16:24:19 -0800

    fix John L is always in the first row issue

--- persistence-with-jdbc/src/com/allen/QueueDays.java
+++ persistence-with-jdbc/src/com/allen/QueueDays.java
@@ -7,20 +7,39 @@ public class QueueDays {
 	public static Map<String, Double> hash = new HashMap<>();
 
 	public static void initHash() {
-		hash.put("Alex", (double) 3);
-		hash.put("Allen", (double) 4);
-		hash.put("April", (double) 3);
-		hash.put("Graham", (double) 4);
-		hash.put("Hitomi", (double) 2.5);
-		hash.put("John H", (double) 0);
-		double johnL = 0.5;
-		hash.put("John L", (double) 0 * johnL);
-		hash.put("Julie", (double) 2);
-		hash.put("Leila", (double) 0);
-		hash.put("Marc", (double) 3);
-		hash.put("Pedro", (double) 3);
-		hash.put("Stefan", (double) 1);
-		double yvonne = 0.75;
-		hash.put("Yvonne", (double) 2 * yvonne);
+		hash.put("Ahaan", (double) 5);
+		
+		hash.put("Alex", (double) 5);
+		hash.put("Allen", (double) 5);
+		hash.put("April", (double) 5);
+		hash.put("Graham", (double) 5);
+		hash.put("Hitomi", (double) 5);
+		hash.put("John H", (double) 5);
+		
+		hash.put("John L", (double) 5);
+		
+		hash.put("Julie", (double) 5);
+		hash.put("Leila", (double) 5);
+		hash.put("Marc", (double) 5);
+		hash.put("Pedro", (double) 5);
+		hash.put("Stefan", (double) 5);
+		
+		hash.put("Yvonne", (double) 5);
+	}
+	
+	public static void changeValue(String name, double value) {
+		hash.put(name, value);
+	}
+	
+	public static void minusValue(String name) {
+		hash.put(name, (double)getValue(name)-0.5);
+	}
+	
+	public static void addValue(String name) {
+		hash.put(name, (double)getValue(name)+0.5);
+	}
+	
+	public static double getValue(String name) {
+		return hash.get(name);
 	}
 }
