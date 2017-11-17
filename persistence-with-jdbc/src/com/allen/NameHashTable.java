cf04676d29219013e4de5c4d2938c7c811753abe
Yilun Qian <qianyilun@gmail.com>
2017-11-16 16:45:15 -0800

    Merge branch 'version_1.2.2'

--- persistence-with-jdbc/src/com/allen/NameHashTable.java
+++ persistence-with-jdbc/src/com/allen/NameHashTable.java
@@ -30,36 +30,3 @@ public class NameHashTable {
 	}
 	
 }
-=======
-package com.allen;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/*
- * Based on the Alpha sort, this following engineers order was hardcoded.
- * 
- * @author Allen Qian
- */
-
-public class NameHashTable {
-	public static Map<String, Integer> hash = new HashMap<>();
-
-	public static void initHash() {
-		hash.put("Alex", 1);
-		hash.put("Allen", 2);
-		hash.put("April", 3);
-		hash.put("Graham", 4);
-		hash.put("Hitomi", 5);
-		hash.put("John H", 6);
-		hash.put("John L", 7);
-		hash.put("Julie", 8);
-		hash.put("Leila", 9);
-		hash.put("Marc", 10);
-		hash.put("Pedro", 11);
-		hash.put("Stefan", 12);
-		hash.put("Yvonne", 13);
-		hash.put("Ahaan", 14);
-	}
-	
-}
